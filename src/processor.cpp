60f549f359517e3d2dc1a58429f093f2dd985420
zbakin <50952098+zbakin@users.noreply.github.com>
2023-05-15 09:08:26 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,8 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <iostream>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+// Return the aggregate CPU utilization
+float Processor::Utilization() {
+  return static_cast<float>(LinuxParser::ActiveJiffies()) / LinuxParser::Jiffies();
+}

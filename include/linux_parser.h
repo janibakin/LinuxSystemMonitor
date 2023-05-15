56a2a9e668f8e928f59afd78d9c7500bc8f02318
zbakin <50952098+zbakin@users.noreply.github.com>
2023-05-15 08:55:14 +0100

    Update linux_parser.h

--- include/linux_parser.h
+++ include/linux_parser.h
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <regex>
 #include <string>
+#include <iostream>
 
 namespace LinuxParser {
 // Paths
@@ -43,7 +44,7 @@ enum CPUStates {
 std::vector<std::string> CpuUtilization();
 long Jiffies();
 long ActiveJiffies();
-long ActiveJiffies(int pid);
+// long ActiveJiffies(int pid);
 long IdleJiffies();
 
 // Processes
